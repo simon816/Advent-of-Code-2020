8001014d5f1a894b60eea52fa29b7c4291542f30
simon816 <commits@simon816.com>
2020-12-15 19:59:26 +0000

    Day 15

---
+++ 15/part2.py
@@ -0,0 +1,25 @@
+starting = list(map(int, input().split(',')))
+
+last_spoken = {}
+
+turn = 1
+last = None
+for n in starting:
+    last_spoken[n] = (turn, None)
+    turn += 1
+    last = n
+
+while turn <= 30000000:
+    last_turn, prev_spoken = last_spoken[last]
+    if prev_spoken is None:
+        last = 0
+    else:
+        last = last_turn - prev_spoken
+    if last in last_spoken:
+        last_spoken_last, _ = last_spoken[last]
+    else:
+        last_spoken_last = None
+    last_spoken[last] = (turn, last_spoken_last)
+    turn += 1
+
+print(last)
