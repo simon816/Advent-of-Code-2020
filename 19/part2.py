461c7afeb58ff75f594c685cd055a80c06191502
simon816 <commits@simon816.com>
2020-12-19 05:30:57 +0000

    Day 19

---
+++ 19/part2.py
@@ -0,0 +1,38 @@
+import sys
+import re
+from lark import Lark
+
+rules, messages = sys.stdin.read().split('\n\n')
+
+grammar = []
+
+for rule in rules.split('\n'):
+    if not rule:
+        continue
+    id, r = rule.split(':')
+    if id == '8':
+        r = '42 | 42 8'
+    if id == '11':
+        r = '42 31 | 42 11 31'
+    r = r.strip()
+    if r.startswith('"'):
+        grammar.append('r_%s: %s' % (id, r))
+    else:
+        grammar.append('r_%s: %s' % (id, re.sub('(\d+)', r'r_\1', r)))
+
+grammar = '\n'.join(grammar)
+parser = Lark(grammar, start='r_0')
+
+match = 0
+
+for m in messages.split('\n'):
+    m = m.strip()
+    if not m:
+        continue
+    try:
+        parser.parse(m)
+        match += 1
+    except:
+        pass
+
+print(match)
